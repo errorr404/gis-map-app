31211f34dc2ca0a29e4bc88c7115b5120bd9321e
errorr404 <dkbishwash@gmail.com>
2023-04-23 23:24:21 +0530

    add markers in road

--- App.js
+++ App.js
@@ -1,12 +1,58 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import MapView, { Circle, Marker, Polygon } from 'react-native-maps';
 import * as Location from 'expo-location';
 import { Button, StyleSheet, View } from 'react-native';
 
+const TYPE = {
+  MARKER:"MARKER",
+  CIRCLE:"CIRCLE",
+  POLYGON:"POLYGON"
+}
+
+const elementsType = [
+  {
+    type:TYPE.MARKER,
+    id:"street-light-0",
+    latitude:28.451753,
+    longitude:77.030546
+  },
+  {
+    type:TYPE.MARKER,
+    id:"street-light-1",
+    latitude:28.451832, 
+    longitude:77.030427
+  },
+  {
+    type:TYPE.MARKER,
+    id:"street-light-2",
+    latitude:28.451897, 
+    longitude:77.029790
+  },
+  {
+    type:TYPE.MARKER,
+    id:"street-light-3",
+    latitude:28.451974,  
+    longitude:77.029210,
+  },
+  {
+    type:TYPE.MARKER,
+    id:"street-light-4",
+    latitude:28.452063, 
+    longitude:77.028731
+  }
+  ,{
+    // type:TYPE.CIRCLE,
+    // id:"circle"
+  },{
+    type:TYPE.POLYGON,
+    id:"polygon"
+  }
+]
+
 export default function App() {
   const [region,setRegion]= useState({
-    latitude: 37.78825,
-    longitude: -122.4324,
+    latitude: 28.445860,
+    longitude: 77.034230,
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
   })
@@ -22,19 +68,20 @@ export default function App() {
     let location = await Location.getCurrentPositionAsync({});
     console.log(location.coords)
     setRegion({
-      latitude:33.5004686,
-      longitude:-111.9027061,
+      latitude:location.coords.latitude,
+      longitude:location.coords.longitude,
       latitudeDelta: 0.0922,
       longitudeDelta: 0.0421
     })
     console.log("location:-->", location.coords.latitude, location.coords.longitude)
   }
   useEffect(() => {
-    userLocation()
+    // userLocation()
   },[])
 
   useEffect(() => {
-    const finalRes = [{
+    const finalRes = [
+      {
       latitude:33.5362475,
       longitude:-111.9267386
     
@@ -59,16 +106,31 @@ export default function App() {
     
     setFinalLatLog(finalRes)
   },[region]);
-  useEffect(() => {
-    console.log("regoion-->",region)
-  },[region])
+
+
+  const getRenderer = useCallback((el) => {
+    const {type, longitude, latitude,id} = el;
+    let tempLocation = {...region};
+    if(longitude && latitude) tempLocation = {...tempLocation, longitude, latitude}
+    switch(type) {
+      case TYPE.MARKER:
+        return   <Marker
+        //  image={{uri:"https://i.ibb.co/GpqVX6h/street-light.png"}} 
+        //   style={{width: 10, height: 10,}}
+           key={id} onPress={() => alert("clicked marker")} coordinate={tempLocation} title="marker" />;
+      case TYPE.CIRCLE:
+        // return <Circle key={id} center={tempLocation} radius={10000} fillColor="#6F5BDC" strokeColor="#000"/>;
+      // case TYPE.POLYGON: 
+      //   return <Polygon key={id} coordinates={logLatArr}  fillColor="#6F5BDC" strokeColor="#000" />
+    }
+  },[region]);
   return (
     <View style={styles.container} >
       <MapView style={styles.map} region={region}>
-
-        <Marker onPress={() => alert("clicked marker")} coordinate={region} title="marker" />
-        {/* <Circle center={region} radius={100} fillColor="#6F5BDC" strokeColor="#000"/> */}
-        <Polygon coordinates={logLatArr}  fillColor="#6F5BDC" strokeColor="#000" />
+        {
+          elementsType.map((el) =>getRenderer(el))
+        }
+       
       </MapView>
       <Button onPress={userLocation} title='Get coordinates'/>
     </View>
